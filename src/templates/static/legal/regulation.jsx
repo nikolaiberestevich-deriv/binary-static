517ef7170145845810158e5f4652690854dd34e3
Nikolai Berestevich <nikolai.berestevich@regentmarkets.com>
2021-11-25 10:57:23 +0300

    nikolai.berestevich/fix/delete iom in regulatiry page

--- src/templates/static/legal/regulation.jsx
+++ src/templates/static/legal/regulation.jsx
@@ -559,7 +559,6 @@ const Regulation = () => {
                     <p>{it.L('Deriv (MX) Ltd, Millennium House, Level 1, Victoria Road, Douglas IM2 4RW, Isle of Man, is licensed and regulated in Great Britain by the Gambling Commission under [_1]account no. 39172.[_1]', '<a href="https://www.gamblingcommission.gov.uk/public-register/business/detail/39172" target="_blank" rel="noopener noreferrer">')}</p>
                     <p>{it.L('Clients in the UK who wish to trade gambling products will have their accounts opened with Deriv (MX) Ltd.')}</p>
                     <RegulatorText />
-                    <RegulatorImage href='https://www.gov.im/gambling/' image='images/pages/regulation/isle-of-man.png' />
                 </Box>
             </div>
 
